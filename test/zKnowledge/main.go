d48cf4a3257dabc7a91d35d3bae1eab6620daa69
nigel007 <609441862@qq.com>
2018-11-24 22:20:34 +0800

    test building go.so lib for python

--- test/zKnowledge/main.go
+++ test/zKnowledge/main.go
@@ -1,20 +1,32 @@
 package main
 
 import (
+	"container/list"
 	"fmt"
 	"math/rand"
+	"C"
 )
 
-func main(){
-	//nodeHash := "QmdfYLM2jQRF6EMWNQwbMeTmqrxw1YAFA4ithj6KctVRZ8"
-	//nodeInt, err := strconv.ParseInt(nodeHash, 10, 64)
-	//if err != nil{
-	//	panic(err)
-	//}
+func randomFunc() *list.List {
 	nodeId := 123
 	rand.NewSource((int64(nodeId)))
+	result := list.New()
 	for i := 0; i < 10; i++{
 		a := rand.Intn(100)
 		fmt.Println(a)
+		result.PushBack(a)
 	}
+	return result
+}
+
+func test(){
+	var s int
+	for a := 0; a <= 1000000; a++ {
+		s += a
+	}
+	fmt.Println(s)
+}
+
+func main(){
+
 }
