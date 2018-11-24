993544bf9dee72f8b90b55786184fe61de37e1eb
nigel007 <609441862@qq.com>
2018-11-24 22:53:48 +0800

    finish testing python call go function

---
+++ test/zKnowledge/backUp.go
@@ -0,0 +1,9 @@
+package main
+
+//rand.NewSource((int64(nodeId)))
+//result := list.New()
+//for i := 0; i < 10; i++{
+//	a := rand.Intn(100)
+//	fmt.Println(a)
+//	result.PushBack(a)
+//}
--- test/zKnowledge/main.go
+++ test/zKnowledge/main.go
@@ -1,32 +1,20 @@
 package main
 
+import "C"
 import (
-	"container/list"
 	"fmt"
 	"math/rand"
-	"C"
 )
 
-func randomFunc() *list.List {
-	nodeId := 123
+//export RandomFunc
+func RandomFunc(nodeId int) {
 	rand.NewSource((int64(nodeId)))
-	result := list.New()
 	for i := 0; i < 10; i++{
 		a := rand.Intn(100)
 		fmt.Println(a)
-		result.PushBack(a)
 	}
-	return result
 }
 
-func test(){
-	var s int
-	for a := 0; a <= 1000000; a++ {
-		s += a
-	}
-	fmt.Println(s)
-}
-
-func main(){
-
+func main() {
+	RandomFunc(123)
 }
